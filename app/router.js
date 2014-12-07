0e74a81d378208ab35e0eaf9406cc0e955f0028b
Mikhail Dubov <mdubov@gmail.com>
2014-12-07 12:34:44 +0000

    Initial setup of chattermill front-end

--- app/router.js
+++ app/router.js
@@ -6,6 +6,25 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('welcome', {path: '/'});
+  this.route('about', {path: '/about'});
+
+  this.resource('user', { path: '/:user_id'});
+  this.resource('companies', function() {
+    this.resource('company', { path: '/:company_id' }, function() {
+      this.resource('subscription', function() {
+        this.route('new');
+        this.route('edit');
+      });
+      this.route('dashboard');
+    });
+  });
+  this.resource('projects', function() {
+    this.route('new');
+    this.resource('project', {path: '/project_id'}, function() {
+      this.resource('themes');
+    });
+  });
 });
 
 export default Router;
